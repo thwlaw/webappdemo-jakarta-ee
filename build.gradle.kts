a733425034c022b880f6d468736f547392af5bd9
Terence Law <terence.law@hgc.com.hk>
2022-02-10 13:07:32 +0800

    Demo project for Jakarta EE webapp

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+/*
+ * This is a general purpose Gradle build.
+ * Learn more about Gradle by exploring our samples at https://docs.gradle.org/7.3.3/samples
+ */
+
+plugins {
+	java
+	war
+}
+
+
+repositories {
+//    mavenCentral()
+}
+
+
+dependencies {
+
+}
+
