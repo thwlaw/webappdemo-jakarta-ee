70ef836cb03eb98f3c0bf8d00a3d12688d5e1c1a
Terence Law <terence.law@hgc.com.hk>
2022-02-14 18:47:44 +0800

    Updated dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,9 +15,9 @@ repositories {
 
 
 dependencies {
-	implementation("org.apache.tomcat:tomcat-servlet-api:10.0.16")
-	implementation("org.apache.tomcat:tomcat-jasper:10.0.16")
-	//implementation("org.apache.tomee:apache-tomee:9.0.0-M")
+	compileOnly("org.apache.tomcat:tomcat-servlet-api:10.0.16")
+	compileOnly("org.apache.tomcat:tomcat-jasper:10.0.16")
+	//compileOnly("org.apache.tomee:apache-tomee:9.0.0-M")
 	implementation("org.hibernate:hibernate-core-jakarta:5.6.5.Final")
 	implementation("mysql:mysql-connector-java:8.0.28")
 }
